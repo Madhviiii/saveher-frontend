86ccb38b03be6be400236e8e91121fb4f34662b8
madhvi sisodiya <sisodiyamadhvi5@gmail.com>
2026-06-24 23:19:58 +0530

    Add twilio config to frontend

---
+++ src/lib/twilio-config.ts
@@ -0,0 +1,10 @@
+// Twilio Configuration
+
+export const TWILIO_CONFIG = {
+  accountSid: '',
+  authToken: '',
+  twilioPhone: '',
+  testMode: true,
+};
+
+// Do not initialize Twilio when credentials are empty
